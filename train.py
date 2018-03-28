474d4d9927ac347bf359b5135e5f74ef745f7714
GPrathap <ggeesara@gmail.com>
2018-03-28 15:18:42 +0300

    configuring gpu

--- train.py
+++ train.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+import os
 import tensorflow as tf
 
 from cifar import networks, data_provider_sattelite
-
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 tfgan = tf.contrib.gan
 flags = tf.flags
 
