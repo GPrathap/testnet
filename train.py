57b99a7eb157f9fb4db34fdfd839beb1e7ab68a5
GPrathap <ggeesara@gmail.com>
2018-04-01 11:58:30 +0300

    increasing depth

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ flags.DEFINE_integer('batch_size', 64, 'The number of images in each batch.')
 
 flags.DEFINE_string('master', '', 'Name of the TensorFlow master to use.')
 
-flags.DEFINE_string('train_log_dir', '/data/satellitegpu/train_log',
+flags.DEFINE_string('train_log_dir', '/data/satellitegpu/train_log1',
                     'Directory where to write event logs.')
 
 flags.DEFINE_string('dataset_dir', '/data/satellitegpu/', 'Location of data.')
