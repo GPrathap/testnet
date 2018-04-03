6910ff1bb0eb6e6908b8886e32bf7fe8d3ce590b
GPrathap <ggeesara@gmail.com>
2018-04-03 10:37:18 +0300

    change the log directory

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ flags.DEFINE_integer('batch_size', 64, 'The number of images in each batch.')
 
 flags.DEFINE_string('master', '', 'Name of the TensorFlow master to use.')
 
-flags.DEFINE_string('train_log_dir', '/data/satellitegpu/train_log2',
+flags.DEFINE_string('train_log_dir', '/data/satellitegpu/train_log4',
                     'Directory where to write event logs.')
 
 flags.DEFINE_string('dataset_dir', '/data/satellitegpu/', 'Location of data.')
