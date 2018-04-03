3fc7961415add3a8e7f555c94a682a360e81a653
GPrathap <ggeesara@gmail.com>
2018-04-03 09:46:33 +0300

    adding a new network

--- cifar/dcgansatelitenew.py
+++ cifar/dcgansatelitenew.py
@@ -20,11 +20,6 @@ from six.moves import xrange
 
 import tensorflow as tf
 from six.moves import xrange
-from tensorlayer.layers import InputLayer, Conv2d, BatchNormLayer, MaxPool2d, FlattenLayer, ConcatLayer, DenseLayer, \
-    ReshapeLayer, DeConv2d
-
-import tensorlayer as tl
-
 slim = tf.contrib.slim
 layers = tf.layers
 
@@ -85,11 +80,6 @@ def discriminator(inputs,
   _validate_image_inputs(inputs)
   inp_shape = inputs.get_shape().as_list()[1]
 
-  k = 5
-  df_dim = 16  # Dimension of discrim filters in first conv layer. [64]
-  w_init = tf.random_normal_initializer(stddev=0.02)
-  gamma_init = tf.random_normal_initializer(1., 0.02)
-
   end_points = {}
   with tf.variable_scope(scope, values=[inputs], reuse=reuse) as scope:
     with slim.arg_scope([normalizer_fn], **normalizer_fn_args):
@@ -98,64 +88,13 @@ def discriminator(inputs,
                           kernel_size=4,
                           activation_fn=tf.nn.leaky_relu):
         net = inputs
-        net_in = InputLayer(inputs, name='d/in')
-        net_h0 = Conv2d(net_in, df_dim, (k, k), (2, 2), act=lambda x: tl.act.lrelu(x, 0.2),
-                        padding='SAME', W_init=w_init, name='d/h0/conv2d')
-        end_points["conv0"] = net_h0
-
-        net_h1 = Conv2d(net_h0, df_dim * 2, (k, k), (2, 2), act=None,
-                        padding='SAME', W_init=w_init, name='d/h1/conv2d')
-        net_h1 = BatchNormLayer(net_h1, act=lambda x: tl.act.lrelu(x, 0.2),
-                                is_train=is_training, gamma_init=gamma_init, name='d/h1/batch_norm')
-        end_points["conv1"] = net_h1
-
-        net_h2 = Conv2d(net_h1, df_dim * 4, (k, k), (2, 2), act=None,
-                        padding='SAME', W_init=w_init, name='d/h2/conv2d')
-        net_h2 = BatchNormLayer(net_h2, act=lambda x: tl.act.lrelu(x, 0.2),
-                                is_train=is_training, gamma_init=gamma_init, name='d/h2/batch_norm')
-        end_points["conv2"] = net_h2
-
-        net_h3 = Conv2d(net_h2, df_dim * 8, (k, k), (2, 2), act=None,
-                        padding='SAME', W_init=w_init, name='d/h3/conv2d')
-        net_h3 = BatchNormLayer(net_h3, act=lambda x: tl.act.lrelu(x, 0.2),
-                                is_train=is_training, gamma_init=gamma_init, name='d/h3/batch_norm')
-        end_points["conv3"] = net_h3
-
-        global_max1 = MaxPool2d(net_h3, filter_size=(4, 4), strides=(1,1), padding='SAME', name='maxpool1')
-        global_max1 = FlattenLayer(global_max1, name='d/h3/flatten')
-
-        net_h4 = Conv2d(net_h3, df_dim * 16, (k, k), (2, 2), act=None,
-                        padding='SAME', W_init=w_init, name='d/h4/conv2d')
-        net_h4 = BatchNormLayer(net_h4, act=lambda x: tl.act.lrelu(x, 0.2),
-                                is_train=is_training, gamma_init=gamma_init, name='d/h4/batch_norm')
-        end_points["conv4"] = net_h4
-
-        global_max2 = MaxPool2d(net_h4, filter_size=(2, 2), strides=(1,1), padding='SAME', name='maxpool2')
-        global_max2 = FlattenLayer(global_max2, name='d/h4/flatten')
-
-        net_h5 = Conv2d(net_h4, df_dim * 32, (k, k), (2, 2), act=None,
-                        padding='SAME', W_init=w_init, name='d/h5/conv2d')
-        net_h5 = BatchNormLayer(net_h5, act=lambda x: tl.act.lrelu(x, 0.2),
-                                is_train=is_training, gamma_init=gamma_init, name='d/h5/batch_norm')
-        end_points["conv5"] = net_h5
-
-        global_max3 = FlattenLayer(net_h5, name='d/h5/flatten')
-
-        feature = ConcatLayer(layers=[global_max1, global_max2, global_max3], name='d/concat_layer1')
-        net_h6 = DenseLayer(feature, n_units=1, act=tf.identity,
-                            W_init=w_init, name='d/h6/lin_sigmoid')
-        end_points["conv6"] = net_h6
-        logits = net_h6.outputs
-        net_h6.outputs = tf.nn.sigmoid(net_h6.outputs)
-        end_points['logits'] = logits
-
-        '''
+        maxpooling = ""
         for i in xrange(int(log(inp_shape, 2))):
           scope = 'conv%i' % (i + 1)
           current_depth = depth * 2**i
           normalizer_fn_ = None if i == 0 else normalizer_fn
           net = slim.conv2d(
-              net, current_depth, normalizer_fn=normalizer_fn_, stride=2, scope=scope)
+              net, current_depth, normalizer_fn=normalizer_fn_, scope=scope)
           if("conv1" == scope):
             maxpooling = layers.max_pooling2d(net, pool_size=[4,4], strides=2)
           end_points[scope] = net
@@ -163,9 +102,7 @@ def discriminator(inputs,
         logits = slim.conv2d(net, 1, kernel_size=1, stride=1, padding='VALID',
                              normalizer_fn=None, activation_fn=None)
         logits = tf.reshape(logits, [-1, 1])
- 
-        '''
-
+        end_points['logits'] = logits
 
         return logits, end_points
 
@@ -212,85 +149,14 @@ def generator(inputs,
       'zero_debias_moving_mean': True,
       'fused': fused_batch_norm,
   }
-  end_points = {}
-  batch_size = 64
+
   inputs.get_shape().assert_has_rank(2)
   if log(final_size, 2) != int(log(final_size, 2)):
     raise ValueError('`final_size` (%i) must be a power of 2.' % final_size)
   if final_size < 8:
     raise ValueError('`final_size` (%i) must be greater than 8.' % final_size)
 
-  image_size = 256
-  k = 4
-  # 128, 64, 32, 16
-  s2, s4, s8, s16, s32, s64 = int(image_size / 2), int(image_size / 4), int(image_size / 8), int(image_size / 16), int(
-      image_size / 32), int(image_size / 64)
-
-  gf_dim = 16  # Dimension of gen filters in first conv layer. [64]
-
-  w_init = tf.random_normal_initializer(stddev=0.02)
-  gamma_init = tf.random_normal_initializer(1., 0.02)
-
-  with tf.variable_scope(scope, reuse=reuse):
-      tl.layers.set_name_reuse(reuse)
-
-      net_in = InputLayer(inputs, name='g/in')
-      net_h0 = DenseLayer(net_in, n_units=gf_dim * 32 * s64 * s64, W_init=w_init,
-                          act=tf.identity, name='g/h0/lin')
-      net_h0 = ReshapeLayer(net_h0, shape=[-1, s64, s64, gf_dim * 32], name='g/h0/reshape')
-      net_h0 = BatchNormLayer(net_h0, act=tf.nn.relu, is_train=is_training,
-                              gamma_init=gamma_init, name='g/h0/batch_norm')
-      scope = 'deconv0'
-      end_points[scope] = net_h0
-
-      net_h1 = DeConv2d(net_h0, gf_dim * 16, (k, k), out_size=(s32, s32), strides=(2, 2),
-                        padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h1/decon2d')
-      net_h1 = BatchNormLayer(net_h1, act=tf.nn.relu, is_train=is_training,
-                              gamma_init=gamma_init, name='g/h1/batch_norm')
-      scope = 'deconv1'
-      end_points[scope] = net_h1
-
-      net_h2 = DeConv2d(net_h1, gf_dim * 8, (k, k), out_size=(s16, s16), strides=(2, 2),
-                        padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h2/decon2d')
-      net_h2 = BatchNormLayer(net_h2, act=tf.nn.relu, is_train=is_training,
-                              gamma_init=gamma_init, name='g/h2/batch_norm')
-      scope = 'deconv2'
-      end_points[scope] = net_h2
-
-      net_h3 = DeConv2d(net_h2, gf_dim * 4, (k, k), out_size=(s8, s8), strides=(2, 2),
-                        padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h3/decon2d')
-      net_h3 = BatchNormLayer(net_h3, act=tf.nn.relu, is_train=is_training,
-                              gamma_init=gamma_init, name='g/h3/batch_norm')
-      scope = 'deconv3'
-      end_points[scope] = net_h3
-
-      net_h4 = DeConv2d(net_h3, gf_dim * 2, (k, k), out_size=(s4, s4), strides=(2, 2),
-                        padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h4/decon2d')
-      net_h4 = BatchNormLayer(net_h4, act=tf.nn.relu, is_train=is_training,
-                              gamma_init=gamma_init, name='g/h4/batch_norm')
-      scope = 'deconv4'
-      end_points[scope] = net_h4
-
-      net_h5 = DeConv2d(net_h4, gf_dim * 1, (k, k), out_size=(s2, s2), strides=(2, 2),
-                        padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h5/decon2d')
-      net_h5 = BatchNormLayer(net_h5, act=tf.nn.relu, is_train=is_training,
-                              gamma_init=gamma_init, name='g/h5/batch_norm')
-      scope = 'deconv5'
-      end_points[scope] = net_h5
-
-      net_h6 = DeConv2d(net_h5, 3, (k, k), out_size=(image_size, image_size), strides=(2, 2),
-                        padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h6/decon2d')
-      logits = net_h6.outputs
-      net_h6.outputs = tf.nn.tanh(net_h6.outputs)
-      scope = 'deconv6'
-      end_points[scope] = net_h6
-
-      scope = 'logits'
-      end_points[scope] = logits
-
-  return logits, end_points
-
-  '''
+  end_points = {}
   num_layers = int(log(final_size, 2)) - 1
   with tf.variable_scope(scope, values=[inputs], reuse=reuse) as scope:
     with slim.arg_scope([normalizer_fn], **normalizer_fn_args):
@@ -337,7 +203,5 @@ def generator(inputs,
             [None, final_size, final_size, num_outputs])
 
         return logits, end_points
-
-  '''
 
 
