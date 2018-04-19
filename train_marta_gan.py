942e34ec0438f6dc146920f61ee781d20fc02671
GPrathap <ggeesara@gmail.com>
2018-04-19 19:19:27 +0300

    changing the generating noise distribution

--- train_marta_gan.py
+++ train_marta_gan.py
@@ -34,8 +34,8 @@ flags.DEFINE_integer("c_dim", 3, "Dimension of image color. [3]")
 flags.DEFINE_integer("sample_step", 500, "The interval of generating sample. [500]")
 flags.DEFINE_integer("save_step", 50, "The interval of saveing checkpoints. [500]")
 flags.DEFINE_string("dataset", "uc_train_256_data", "The name of dataset [celebA, mnist, lsun]")
-flags.DEFINE_string("checkpoint_dir", "/data/checkpoint1", "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("sample_dir", "/data/samples", "Directory name to save the image samples [samples]")
+flags.DEFINE_string("checkpoint_dir", "/data/checkpoint2", "Directory name to save the checkpoints [checkpoint]")
+flags.DEFINE_string("sample_dir", "/data/samples2", "Directory name to save the image samples [samples]")
 flags.DEFINE_boolean("is_train", True, "True for training, False for testing [False]")
 flags.DEFINE_boolean("is_crop", False, "True for training, False for testing [False]")
 flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
@@ -156,8 +156,8 @@ def main(_):
                 # get real images
                 batch = [get_image(batch_file, FLAGS.image_size, is_crop=FLAGS.is_crop, resize_w=FLAGS.output_size, is_grayscale = 0) for batch_file in batch_files]
                 batch_images = np.array(batch).astype(np.float32)
-                # batch_z = np.random.uniform(low=-1, high=1, size=(FLAGS.batch_size, z_dim)).astype(np.float32)
-                batch_z = np.transpose(create_mine_grid( 1, z_dim, FLAGS.batch_size, 99, None, True, True))
+                batch_z = np.random.uniform(low=-1, high=1, size=(FLAGS.batch_size, z_dim)).astype(np.float32)
+                #batch_z = np.transpose(create_mine_grid( 1, z_dim, FLAGS.batch_size, 99, None, True, True))
                 start_time = time.time()
                 # updates the discriminator
                 errD, _ = sess.run([d_loss, d_optim], feed_dict={z: batch_z, real_images: batch_images })
