a38c384c43fed83cc1d606d1090cc24bc1aeba0e
GPrathap <ggeesara@gmail.com>
2018-04-21 17:00:03 +0300

    suspected problems with batch normalization: move to layers.batch_normalization

--- network1.py
+++ network1.py
@@ -19,6 +19,12 @@ def conditional_generator_simplified_api(inputs, condition, batch_size, is_train
     conditinal_input = tf.concat([inputs, condition], 0)
     return generator_simplified_api(conditinal_input, batch_size, is_train, reuse)
 
+
+def batch_normalization_layer(layer, gamma_init, scope):
+    layer = tf.layers.batch_normalization(layer, epsilon=1e-5, gamma_initializer=gamma_init,
+                                           name=scope,  trainable=False)
+    return tf.nn.leaky_relu(layer, 0.2)
+
 def generator_simplified_api(inputs, batch_size, is_train=True, reuse=False):
     image_size = 256
     k = 4
@@ -38,44 +44,32 @@ def generator_simplified_api(inputs, batch_size, is_train=True, reuse=False):
             activation_fn=tf.identity, weights_initializer=w_init, reuse=reuse,
             trainable=is_train, scope='g/h0/fully_connected')
         net_h0 = tf.reshape(net_h0, shape=(-1, s64, s64, gf_dim*32))
-        net_h0 = tf.layers.batch_normalization(net_h0, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='g/h0/batch_norm')
-        net_h0 = tf.nn.leaky_relu(net_h0, 0.2)
+        net_h0 = batch_normalization_layer(net_h0, gamma_init, 'g/h0/batch_norm')
 
         net_h1 = tf.contrib.layers.conv2d_transpose(
             net_h0, gf_dim*16, [k, k], stride=2, padding='SAME', activation_fn=None, weights_initializer=w_init,
             reuse=reuse, trainable=is_train, scope='g/h1/conv2d_transpose')
-        net_h1 = tf.layers.batch_normalization(net_h1, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='g/h1/batch_norm')
-        net_h1 = tf.nn.leaky_relu(net_h1, 0.2)
+        net_h1 = batch_normalization_layer(net_h1, gamma_init, 'g/h1/batch_norm')
 
         net_h2 = tf.contrib.layers.conv2d_transpose(
             net_h1, gf_dim * 8, [k, k], stride=2, padding='SAME', activation_fn=None, weights_initializer=w_init,
             reuse=reuse, trainable=is_train, scope='g/h2/conv2d_transpose')
-        net_h2 = tf.layers.batch_normalization(net_h2, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='g/h2/batch_norm')
-        net_h2 = tf.nn.leaky_relu(net_h2, 0.2)
+        net_h2 = batch_normalization_layer(net_h2, gamma_init, 'g/h2/batch_norm')
 
         net_h3 = tf.contrib.layers.conv2d_transpose(
             net_h2, gf_dim * 4, [k, k], stride=2, padding='SAME', activation_fn=None, weights_initializer=w_init,
             reuse=reuse, trainable=is_train, scope='g/h3/conv2d_transpose')
-        net_h3 = tf.layers.batch_normalization(net_h3, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='g/h3/batch_norm')
-        net_h3 = tf.nn.leaky_relu(net_h3, 0.2)
+        net_h3 = batch_normalization_layer(net_h3, gamma_init, 'g/h3/batch_norm')
 
         net_h4 = tf.contrib.layers.conv2d_transpose(
             net_h3, gf_dim * 2, [k, k], stride=2, padding='SAME', activation_fn=None, weights_initializer=w_init,
             reuse=reuse, trainable=is_train, scope='g/h4/conv2d_transpose')
-        net_h4 = tf.layers.batch_normalization(net_h4, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='g/h4/batch_norm')
-        net_h4 = tf.nn.leaky_relu(net_h4, 0.2)
+        net_h4 = batch_normalization_layer(net_h4, gamma_init, 'g/h4/batch_norm')
 
         net_h5 = tf.contrib.layers.conv2d_transpose(
             net_h4, gf_dim * 1, [k, k], stride=2, padding='SAME', activation_fn=None, weights_initializer=w_init,
             reuse=reuse, trainable=is_train, scope='g/h5/conv2d_transpose')
-        net_h5 = tf.layers.batch_normalization(net_h5, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='g/h5/batch_norm')
-        net_h5 = tf.nn.leaky_relu(net_h5, 0.2)
+        net_h5 = batch_normalization_layer(net_h5, gamma_init, 'g/h5/batch_norm')
 
         net_h6 = tf.contrib.layers.conv2d_transpose(
             net_h5, 3, [k, k], stride=2, padding='SAME', activation_fn=None, weights_initializer=w_init,
@@ -107,23 +101,17 @@ def discriminator_simplified_api(inputs, is_train=True, reuse=False):
         net_h1 = tf.contrib.layers.conv2d(net_h1, df_dim*2, [k, k], stride=2, padding='SAME',
                                           weights_initializer=w_init, reuse=reuse, trainable=is_train,
                                           activation_fn=None, scope='d/h1/conv2d')
-        net_h1 = tf.layers.batch_normalization(net_h1, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='d/h1/batch_norm')
-        net_h1 = tf.nn.leaky_relu(net_h1, 0.2)
+        net_h1 = batch_normalization_layer(net_h1, gamma_init, 'd/h1/batch_norm')
 
         net_h2 = tf.contrib.layers.conv2d(net_h1, df_dim * 4, [k, k], stride=2, padding='SAME',
                                           weights_initializer=w_init, reuse=reuse, trainable=is_train,
                                           activation_fn=None, scope='d/h2/conv2d')
-        net_h2 = tf.layers.batch_normalization(net_h2, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='d/h2/batch_norm')
-        net_h2 = tf.nn.leaky_relu(net_h2, 0.2)
+        net_h2 = batch_normalization_layer(net_h2, gamma_init, 'd/h2/batch_norm')
 
         net_h3 = tf.contrib.layers.conv2d(net_h2, df_dim * 8, [k, k], stride=2, padding='SAME',
                                           weights_initializer=w_init, reuse=reuse, trainable=is_train,
                                           activation_fn=None, scope='d/h3/conv2d')
-        net_h3 = tf.layers.batch_normalization(net_h3, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='d/h3/batch_norm')
-        net_h3 = tf.nn.leaky_relu(net_h3, 0.2)
+        net_h3 = batch_normalization_layer(net_h3, gamma_init, 'd/h3/batch_norm')
 
         global_max1 = tf.contrib.layers.max_pool2d( net_h3, [4,4], stride=1, padding='SAME',
                                                     outputs_collections=None, scope='d/h3/max_pool2d')
@@ -132,9 +120,7 @@ def discriminator_simplified_api(inputs, is_train=True, reuse=False):
         net_h4 = tf.contrib.layers.conv2d(net_h3, df_dim * 16, [k, k], stride=2, padding='SAME',
                                           weights_initializer=w_init, reuse=reuse, trainable=is_train,
                                           activation_fn=None, scope='d/h4/conv2d')
-        net_h4 = tf.layers.batch_normalization(net_h4, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='d/h4/batch_norm')
-        net_h4 = tf.nn.leaky_relu(net_h4, 0.2)
+        net_h4 = batch_normalization_layer(net_h4, gamma_init, 'd/h4/batch_norm')
 
         global_max2 = tf.contrib.layers.max_pool2d(net_h4, [2, 2], stride=1, padding='SAME',
                                                    outputs_collections=None, scope='d/h4/max_pool2d')
@@ -143,9 +129,7 @@ def discriminator_simplified_api(inputs, is_train=True, reuse=False):
         net_h5 = tf.contrib.layers.conv2d(net_h4, df_dim * 32, [k, k], stride=2, padding='SAME',
                                           weights_initializer=w_init, reuse=reuse, trainable=is_train,
                                           activation_fn=None, scope='d/h5/conv2d')
-        net_h5 = tf.layers.batch_normalization(net_h5, epsilon=1e-5, gamma_initializer=gamma_init,
-                                               name='d/h5/batch_norm')
-        net_h5 = tf.nn.leaky_relu(net_h5, 0.2)
+        net_h5 = batch_normalization_layer(net_h5, gamma_init, 'd/h5/batch_norm')
 
         global_max3 = tf.contrib.layers.flatten(net_h5, scope='d/h5/flatten')
         feature = tf.concat([global_max1, global_max2, global_max3], axis=1, name='d/h5/concat')
