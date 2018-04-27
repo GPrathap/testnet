7db168810c786547c266b4494f92c77784730776
GPrathap <ggeesara@gmail.com>
2018-04-27 13:35:52 +0300

    adding inception v4

--- train_marta_gan1.py
+++ train_marta_gan1.py
@@ -148,7 +148,8 @@ def main(_):
                     batch_z = np.random.uniform(low=-1, high=1, size=(FLAGS.batch_size, z_dim)).astype(np.float32)
                     start_time = time.time()
 
-                    errD, _ = sess.run([d_loss, d_optim], feed_dict={z: batch_z, real_images: batch_images })
+                    for _ in range(4):
+                        errD, _ = sess.run([d_loss, d_optim], feed_dict={z: batch_z, real_images: batch_images })
                     for _ in range(2):
                         errG, _ = sess.run([g_loss, g_optim], feed_dict={z: batch_z, real_images: batch_images})
                     print("Epoch: [%2d/%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
