307ef23e990975896b4085a90f9afa4db5c5ffed
GPrathap <ggeesara@gmail.com>
2018-05-30 20:33:26 +0300

    changing the dropout layers

--- train_svm1.py
+++ train_svm1.py
@@ -3,6 +3,7 @@ from sklearn.metrics import accuracy_score
 from sklearn import svm, linear_model
 import numpy as np
 from Config import *
+from six.moves import cPickle as pickle
 
 acc = []
 nums = np.load('{}/features.npy'.format(FLAGS.feature_dir))
@@ -18,15 +19,20 @@ for num in nums:
     C = 1000.0
     clf = svm.SVC(kernel='linear', C=C).fit(X_train, y_train)
     print("done in %0.3fs" % (time() - t0))
-
+    clf = pickle.dumps(clf)
     print("Hard mining")
     y_pred = clf.predict(X_train)
+    print("Predicting on training ...")
+    print("Accuracy: %.3f" % (accuracy_score(X_train, y_pred)))
+
     difference = np.where((y_pred-y_train) !=0)
     hard_y_train = y_train[difference]
     hard_x_train = X_train[difference]
+
     clf = clf.fit(hard_x_train, hard_y_train)
-    print("Predicting...")
+
     t0 = time()
+    print("Predicting on testing ...")
     y_pred = clf.predict(X_test)
 
     print ("Accuracy: %.3f" % (accuracy_score(y_test, y_pred)))
