4a3caf07b739838f0252c6947bca19c9c2d6885a
GPrathap <ggeesara@gmail.com>
2018-05-31 10:55:39 +0300

    changing the dropout layers probability

--- network.py
+++ network.py
@@ -153,7 +153,7 @@ class Neotx():
             #feature_set.append(tf.concat(self.get_neoxt_features(net_h6), axis=1))
 
             feature = tf.concat(feature_set, axis=1)
-            feature = tf.nn.dropout(feature, keep_prob=0.85)
+            feature = tf.nn.dropout(feature, keep_prob=0.90)
             net_h7 = tf.layers.dense(feature, 1, activation=tf.identity)
             logits = net_h7
             net_h7 = tf.nn.sigmoid(net_h7)
